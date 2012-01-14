042278a259cdcb2a81e1e8d2135035483103c75a
misfo <tedwardo2@gmail.com>
2012-01-14 14:37:49 -0600

    Add evaluate selection command
    Change commands to use one command class that takes arguments.

--- User/clojure.py
+++ User/clojure.py
@@ -23,6 +23,51 @@ def output_to_view(v, output):
 	v.insert(edit, 0, output)
 	v.end_edit(edit)
 
+def output_to_scratch_buffer(window, name, output):
+	v = window.new_file()
+	v.set_name(name)
+	v.set_scratch(True)
+	v.set_syntax_file('Packages/Clojure/Clojure.tmLanguage')
+	output_to_view(v, output)
+
+def output_to_panel(window, output):
+	v = window.get_output_panel('clojure_output')
+	output_to_view(v, output)
+	window.run_command("show_panel", {"panel": "output.clojure_output"})
+
+def exit_with_error(window, message):
+	output_to_panel(window, message)
+	sys.exit(1)
+
+class LazyViewString:
+	def __init__(self, view):
+		self.view = view
+
+	def __str__(self):
+		if not hasattr(self, '_string_value'):
+			self._string_value = self.get_string()
+		return self._string_value
+
+class Selection(LazyViewString):
+	def get_string(self):
+		print "Selection#get_string"
+		sel = self.view.sel()
+		if len(sel) == 1:
+			return self.view.substr(self.view.sel()[0])
+		else:
+			exit_with_error(self.view.window(), "There must be one selection to evaluate")
+
+class SymbolUnderCursor(LazyViewString):
+	def get_string(self):
+		print "SymbolUnderCursor#get_string"
+		begin = end = self.view.sel()[0].begin()
+		while symbol_char(self.view.substr(begin - 1)): begin -= 1
+		while symbol_char(self.view.substr(end)): end += 1
+		if begin == end:
+			exit_with_error(self.view.window(), "No symbol found under cursor")
+		else:
+			return self.view.substr(sublime.Region(begin, end))
+
 class LeinReplSocket:
 	def __init__(self, port):
 		self.sock = socket.socket()
@@ -41,61 +86,47 @@ class LeinReplSocket:
 		self.sock.close()
 
 
-class ClojureReplCommand(sublime_plugin.TextCommand):
+class ClojureEvaluate(sublime_plugin.TextCommand):
 	def _repl_port_number(self):
-		folders = self.view.window().folders()
+		window = self.view.window()
+		folders = window.folders()
 		file_name = self.view.file_name()
 		try:
 			proj_folder = (f for f in folders if file_name.startswith(f)).next()
 		except StopIteration:
-			self._output_to_panel("No folder open containing " + file_name)
-			sys.exit(1)
+			exit_with_error(window, "No folder open containing " + file_name)
 
 		project_clj_file_name = os.path.join(proj_folder, 'project.clj')
 		try:
 			project_clj = open(project_clj_file_name, 'r').read()
 		except IOError:
-			self._output_to_panel("No project.clj found in " + proj_folder)
-			sys.exit(1)
+			exit_with_error(window, "No project.clj found in " + proj_folder)
 
 		repl_port = find_hash_value(":repl-port", project_clj)
 		if repl_port:
 			return int(repl_port)
 		else:
-			self._output_to_panel("No :repl-port specified in " + project_clj_file_name)
-			sys.exit(1)
+			exit_with_error(window, "No :repl-port specified in " + project_clj_file_name)
 
-	def symbol_under_cursor(self):
-		begin = end = self.view.sel()[0].begin()
-		while symbol_char(self.view.substr(begin - 1)): begin -= 1
-		while symbol_char(self.view.substr(end)): end += 1
-		return self.view.substr(sublime.Region(begin, end))
-
-	def _output_to_buffer(self, name, output):
-		v = self.view.window().new_file()
-		v.set_name(name)
-		v.set_scratch(True)
-		v.set_syntax_file('Packages/Clojure/Clojure.tmLanguage')
-		output_to_view(v, output)
-
-	def _output_to_panel(self, output):
-		v = self.view.window().get_output_panel('clojure_output')
-		output_to_view(v, output)
-		self.view.window().run_command("show_panel", {"panel": "output.clojure_output"})
-
-	def evaluate_form(self, edit, expr, use_buffer = False, strip_nil_return = False):
+	def run(self, edit, form, in_current_ns=None, use_buffer=False, strip_nil_return=False):
+		window = self.view.window()
 		try:
 			port = self._repl_port_number()
 			s = LeinReplSocket(port)
 		except socket.error:
-			self._output_to_panel("No repl is listenting on port " + str(port) + "\nPlease start one with `lein repl`")
+			exit_with_error(window, "No repl is listenting on port " + str(port) + "\nPlease start one with `lein repl`")
+
+		template = string.Template(form)
+		expr = template.substitute({
+			"selection": Selection(self.view),
+			"symbol_under_cursor": SymbolUnderCursor(self.view)})
 
 		(output, prompt) = s.send(None)
 		print "prompt before everything", prompt
 
 		file_name = self.view.file_name()
 		forms = []
-		if file_name:
+		if in_current_ns and file_name:
 			path = classpath_relative_path(file_name)
 			forms.append("(load \"/" + path + "\")")
 
@@ -108,25 +139,12 @@ class ClojureReplCommand(sublime_plugin.TextCommand):
 		s.close()
 
 		if not output:
-			self._output_to_panel("There was an error while executing " + do_form)
-			return
+			exit_with_error(window, "There was an error while executing " + do_form)
 
 		if strip_nil_return:
 			output = re.sub(r"\nnil$", "", output)
 
 		if use_buffer:
-			self._output_to_buffer(expr, output)
+			output_to_scratch_buffer(window, expr, output)
 		else:
-			self._output_to_panel(prompt + do_form + "\n" + output)
-
-class SymbolDocumentation(ClojureReplCommand):
-	def run(self, edit):
-		symbol = self.symbol_under_cursor()
-		if not symbol: return
-		self.evaluate_form(edit, "(clojure.repl/doc " + symbol + ")")
-
-class SymbolSource(ClojureReplCommand):
-	def run(self, edit):
-		symbol = self.symbol_under_cursor()
-		if not symbol: return
-		self.evaluate_form(edit, "(clojure.repl/source " + symbol + ")", use_buffer = True, strip_nil_return = True)
+			output_to_panel(window, prompt + do_form + "\n" + output)
