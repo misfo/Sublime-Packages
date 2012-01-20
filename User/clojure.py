4d2e0b763c2aec34cc5e5c36cc9369556204a5c3
misfo <tedwardo2@gmail.com>
2012-01-19 21:51:40 -0600

    Evaluate expression on a different thread

--- User/clojure.py
+++ User/clojure.py
@@ -1,4 +1,5 @@
-import re, os, socket, string, sublime, sublime_plugin
+import re, os, socket, string, sublime, sublime_plugin, threading
+from functools import partial
 
 max_cols = 60
 
@@ -6,7 +7,7 @@ def clean(str):
 	return str.translate(None, '\r') if str else None
 
 def symbol_char(char):
-	return re.match("[-\w*+!?/.]", char)
+	return re.match("[-\w*+!?/.<>]", char)
 
 def classpath_relative_path(file_name):
 	(abs_path, ext) = os.path.splitext(file_name)
@@ -20,16 +21,18 @@ def find_hash_value(key, clj):
 	match = re.search(key + r"[\s\n]+(\d+)", clj)
 	return match.group(1) if match else None
 
-def output_to_view(v, ns, form, output):
+def output_to_view(v, text):
 	v.set_read_only(False)
 	edit = v.begin_edit()
-	convo = "; " + ns + "=> " + ";" * (max_cols - 5 - len(ns)) + "\n" + \
-		    form + "\n" + \
-		    ";" * max_cols + "\n" + \
-		    output + "\n\n"
-	v.insert(edit, v.size(), convo)
+	v.insert(edit, v.size(), text)
 	v.end_edit(edit)
 	v.set_read_only(True)
+
+def output_result_to_view(v, result):
+	output_to_view(v, "; " + result['ns'] + "=> " + ";" * (max_cols - 5 - len(result['ns'])) + "\n" +
+					  result['expr'] + "\n" +
+					  ";" * max_cols + "\n" +
+					  result['output'] + "\n\n")
 
 def exit_with_status(message):
 	sublime.status_message(message)
@@ -38,6 +41,12 @@ def exit_with_status(message):
 def exit_with_error(message):
 	sublime.error_message(message)
 	sys.exit(1)
+
+def call_after_thread_dies(func, thread):
+	if thread.is_alive():
+		sublime.set_timeout(partial(call_after_thread_dies, func, thread), 100)
+	else:
+		func()
 
 class LazyViewString:
 	def __init__(self, view):
@@ -50,7 +59,6 @@ class LazyViewString:
 
 class Selection(LazyViewString):
 	def get_string(self):
-		print "Selection#get_string"
 		sel = self.view.sel()
 		if len(sel) == 1:
 			return self.view.substr(self.view.sel()[0]).strip()
@@ -59,7 +67,6 @@ class Selection(LazyViewString):
 
 class SymbolUnderCursor(LazyViewString):
 	def get_string(self):
-		print "SymbolUnderCursor#get_string"
 		begin = end = self.view.sel()[0].begin()
 		while symbol_char(self.view.substr(begin - 1)): begin -= 1
 		while symbol_char(self.view.substr(end)): end += 1
@@ -68,13 +75,13 @@ class SymbolUnderCursor(LazyViewString):
 		else:
 			return self.view.substr(sublime.Region(begin, end))
 
-class LeinReplSocket:
-	def __init__(self, port):
-		self.sock = socket.socket()
-		self.sock.connect(('localhost', port))
-		self.sock.settimeout(2)
+class Repler(threading.Thread):
+	def __init__(self, sock, exprs):
+		threading.Thread.__init__(self)
+		self.sock = sock
+		self.exprs = exprs
 
-	def send(self, expr):
+	def _send(self, expr):
 		if expr: self.sock.send(expr + "\n")
 		output = "" # TODO use buffer instead?
 		while 1:
@@ -82,8 +89,18 @@ class LeinReplSocket:
 			match = re.match(r"(.*\n)?(\S+)=> $", output, re.DOTALL)
 			if match: return (clean(match.group(1)), match.group(2))
 
-	def close(self):
+	def run(self):
+		self.results = []
+
+		_, ns = self._send(None)
+
+		for expr in self.exprs:
+			output, next_ns = self._send(expr)
+			self.results.append({'ns': ns, 'expr': expr, 'output': output})
+			ns = next_ns
+
 		self.sock.close()
+		self.resulting_ns = ns
 
 
 class ClojureEvaluate(sublime_plugin.TextCommand):
@@ -110,77 +127,78 @@ class ClojureEvaluate(sublime_plugin.TextCommand):
 			exit_with_error("No :repl-port specified in " + project_clj_file_name)
 
 	def run(self, edit, form, in_current_ns=None, use_buffer=False, strip_nil_return=False):
-		window = self.view.window()
-		port = None
-		repl_view = None
+		self._window = self.view.window()
+		self._port = None
+		self._repl_view = None
 		repl_ns = None
-		for v in window.views():
+		for v in self._window.views():
 			match = re.match(r"\(in-ns '(.+)\)", v.name())
 			if match:
 				repl_ns = match.group(1)
-				repl_view = v
-				port = repl_view.settings().get('clojure_repl_port')
-				print "repl_ns", repl_ns
-				print "repl_view", repl_view
-				print "port", port
+				self._repl_view = v
+				self._port = self._repl_view.settings().get('clojure_repl_port')
 				break
 
-		if not port:
-			port = self._repl_port_number()
+		if not self._port:
+			self._port = self._repl_port_number()
 
 		try:
-			s = LeinReplSocket(port)
+			sock = socket.socket()
+			sock.connect(('localhost', self._port))
+			sock.settimeout(10)
 		except socket.error:
-			exit_with_error("No repl is listening on port " + str(port) + "\nPlease start one with `lein repl`")
-
-		if not repl_view:
-			repl_view = window.new_file()
-			repl_view.set_scratch(True)
-			# TODO: put connection details in comment at top of view
-			repl_view.set_syntax_file('Packages/Clojure/Clojure.tmLanguage')
-			repl_view.settings().set('scroll_past_end', True)
-			repl_view.settings().set('clojure_repl_port', port)
-			repl_view.set_read_only(True)
-
-		starting_pt = repl_view.size()
+			exit_with_error("No repl is listening on port " + str(self._port) + "\nPlease start one with `lein repl`")
 
 		template = string.Template(form)
-		form = template.substitute({
+		expr = template.substitute({
 			"selection": Selection(self.view),
 			"symbol_under_cursor": SymbolUnderCursor(self.view)})
 
-		_, initial_ns = s.send(None)
-		if not repl_ns: repl_ns = initial_ns
-		prev_ns = initial_ns
+		exprs = []
 
 		file_name = self.view.file_name()
 		if in_current_ns and file_name:
 			path = classpath_relative_path(file_name)
 			file_ns = re.sub("/", ".", path)
-			load_form = "(load \"/" + path + "\")"
-			output, prev_ns = s.send(load_form)
-			output_to_view(repl_view, initial_ns, load_form, output)
-			form = "(binding [*ns* (find-ns '" + file_ns + ")]\n  " + form + ")"
+			exprs.append("(load \"/" + path + "\")")
+			exprs.append("(binding [*ns* (find-ns '" + file_ns + ")]\n  " + expr + ")")
+		else:
+			exprs.append(expr)
 
-		main_form_pt = repl_view.size() + max_cols + 1
-		output, last_ns = s.send(form)
-		# TODO comment out (?) all output before the return value
+		self._thread = Repler(sock, exprs)
+		self._thread.start()
+		call_after_thread_dies(self._handle_results, self._thread)
 
-		s.close()
+	def _handle_results(self):
+		if not self._repl_view:
+			self._repl_view = self._window.new_file()
+			self._repl_view.set_scratch(True)
+			# TODO: put connection details in comment at top of view
+			self._repl_view.set_syntax_file('Packages/Clojure/Clojure.tmLanguage')
+			self._repl_view.settings().set('scroll_past_end', True)
+			self._repl_view.settings().set('clojure_repl_port', self._port)
+			self._repl_view.set_read_only(True)
+			output_to_view(self._repl_view, "; Connected to Clojure REPL on port " +
+											str(self._port) + "\n\n")
 
-		repl_view.set_name("(in-ns '" + last_ns + ")")
+		starting_pt = self._repl_view.size()
+		main_form_pt = starting_pt + max_cols + 1
 
-		output_to_view(repl_view, prev_ns, form, output)
+		for result in self._thread.results:
+			output_result_to_view(self._repl_view, result)
 
-		repl_view.set_viewport_position(repl_view.text_to_layout(starting_pt))
-		repl_view.sel().clear()
-		repl_view.sel().add(sublime.Region(main_form_pt))
-		window.run_command('toggle_bookmark')
+		self._repl_view.sel().clear()
+		self._repl_view.sel().add(sublime.Region(main_form_pt))
+		self._window.run_command('toggle_bookmark')
 
-		active_view = window.active_view()
-		active_group = window.active_group()
-		repl_view_group, _ = window.get_view_index(repl_view)
-		window.focus_view(repl_view)
+		self._repl_view.set_name("(in-ns '" + self._thread.resulting_ns + ")")
+
+		active_view = self._window.active_view()
+		active_group = self._window.active_group()
+		repl_view_group, _ = self._window.get_view_index(self._repl_view)
+		self._window.focus_view(self._repl_view)
 		if repl_view_group != active_group:
 			# give focus back to the originally active view if it's in a different group
-			window.focus_view(active_view)
+			self._window.focus_view(active_view)
+
+		self._repl_view.set_viewport_position(self._repl_view.text_to_layout(starting_pt))
